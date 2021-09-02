2f7a1723b24daba0ba85016060f6408ac9b8e46d
Gabrielzyr <gamonte-@student.42sp.org.br>
2021-09-02 18:58:33 -0300

    Fixed: ft_strrchr.c

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -4,18 +4,12 @@ char	*ft_strrchr(const char *s, int c)
 {
 	int	s_len;
 
-	if (s)
+	s_len = ft_strlen(s);
+	while (s_len >= 0)
 	{
-		s_len = ft_strlen(s);
-		if (s_len != 0)
-		{
-			while (s_len >= 0)
-			{
-				if ((char)c == s[s_len])
-					return ((char *)s + s_len);
-				s_len--;
-			}
-		}
+		if ((char)c == s[s_len])
+			return ((char *)s + s_len);
+		s_len--;
 	}
 	return (0);
 }
